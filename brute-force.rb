4189d9e671b3d9387f0b51a16a8b9aac43b595b7
suomiseth <suomiseth@gmail.com>
2015-10-26 23:13:20 -0400

    pulled out some stats from the GMAT

---
+++ brute-force.rb
@@ -0,0 +1,22 @@
+class FortySquares
+
+  def initialize(n, c)
+    @n = n
+    @c = c
+  end
+
+  def combos_no_repetition
+    (1..@n).to_a.inject(:*) / ((1..@c).to_a.inject(:*) * (1..(@n - @c)).to_a.inject(:*))
+  end
+end
+
+
+puts "give an n and a c"
+puts "n reps the total number of possible moves"
+puts "n:"
+n = gets.strip
+puts "c reps the number of moves required to reach the desitnation"
+puts "c:"
+c = gets.strip
+
+puts FortySquares.new(n.to_i, c.to_i).combos_no_repetition
